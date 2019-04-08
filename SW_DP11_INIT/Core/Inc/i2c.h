fc8e37329acc7384e9a13f3c9e102d7067fcfef0
IshacOursana <ishac.oursana@gmail.com>
2019-04-08 19:38:55 +0200

    software base + eeprom

--- SW_DP11_INIT/Core/Inc/i2c.h
+++ SW_DP11_INIT/Core/Inc/i2c.h
@@ -58,6 +58,8 @@
 
 /* USER CODE BEGIN Includes */
 
+#define EEPROM_ADDRESS 0xA0 	 
+	 
 /* USER CODE END Includes */
 
 extern I2C_HandleTypeDef hi2c1;
@@ -70,6 +72,9 @@ void MX_I2C1_Init(void);
 
 /* USER CODE BEGIN Prototypes */
 
+void I2C_eeprom_write(uint8_t , uint16_t);	//si passa l'indice dell'inidicatore e l'indirizzo della memmoria
+uint8_t I2C_eeprom_read(uint16_t);		// ritorna l'indice dell'indicatore da visualizzare
+	 
 /* USER CODE END Prototypes */
 
 #ifdef __cplusplus
--- SW_DP11_INIT/Core/Src/freertos.c
+++ SW_DP11_INIT/Core/Src/freertos.c
@@ -58,6 +58,7 @@
 /* USER CODE BEGIN Includes */     
 
 #include "general.h"
+#include "i2c.h"
 
 /* USER CODE END Includes */
 
@@ -566,13 +567,22 @@ void downShiftTask(void const * argument)
 * @retval None
 */
 /* USER CODE END Header_modeSelectorTask */
-void modeSelectorTask(void const * argument)
+void modeSelectorTask(void const * argument)	//-----------------------INSERIRE LA CHIAMATA ALLA TASK---------------------------
 {
   /* USER CODE BEGIN modeSelectorTask */
   /* Infinite loop */
   for(;;)
   {
 		xSemaphoreTake(modeSelectorSemaphoreHandle, portMAX_DELAY);
+		uint8_t indirizzo = 10;
+		I2C_eeprom_write (indirizzo , 0);			// mando il valore 10 nell'inidirizzo 0 della eeprom
+		if ( 10 == I2C_eeprom_read (0)){			// se leggo il valore 10 nell'indirizzo 0 della eeprom
+			HAL_GPIO_WritePin ( DEBUG_LED_2_GPIO_Port, DEBUG_LED_2_Pin, GPIO_PIN_SET);
+		}
+		else{
+			HAL_GPIO_WritePin ( DEBUG_LED_2_GPIO_Port, DEBUG_LED_2_Pin, GPIO_PIN_RESET);
+		}
+		
     osDelay(1);
   }
   /* USER CODE END modeSelectorTask */
